6f04c7cb5c3c872dedbec744f6ddc9b4cefd2638
Stephen Arnold <smarnold@me.com>
2021-04-26 09:12:01 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 let degrees = 0
 basic.forever(function () {
     degrees = input.compassHeading()
-    if (degrees < 45) {
+    if (degrees < 3 || degrees > 357) {
         basic.showString("N")
-    } else if (degrees < 135) {
+    } else if (degrees < 93 && degrees > 87) {
         basic.showString("E")
-    } else if (degrees < 225) {
+    } else if (degrees < 183 && degrees > 177) {
         basic.showString("S")
-    } else if (degrees < 315) {
+    } else if (degrees < 273 && degrees > 267) {
         basic.showString("W")
     } else {
-        basic.showString("N")
+        basic.showString("" + (degrees))
     }
 })
